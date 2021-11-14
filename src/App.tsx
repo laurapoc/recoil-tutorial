355e78ebdd2438e8a82395a5d494041316a295af
Laura Pociuniene <laurapociuniene@gmail.com>
2021-11-14 15:30:18 +0200

    render and output repositories

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
 import { useEffect } from "react";
+import { atom, useRecoilState } from "recoil";
 
-function App() {
+const repoState = atom({
+  key: "repos",
+  default: [],
+});
+
+function App(): JSX.Element {
+  const [repos, setRepos] = useRecoilState(repoState);
+
   useEffect(() => {
     const getRepos = async () => {
       const url = "https://api.github.com/users/laurapoc/repos";
       const resp = await fetch(url);
       const body = await resp.json();
-      console.log(body);
+      setRepos(body);
     };
 
     getRepos();
-  }, []);
+  }, [setRepos]);
 
-  return <div></div>;
+  console.log(repos);
+
+  return (
+    <>
+      {repos.map((repo: any) => {
+        console.log(repo);
+        return (
+          <div key={repo.url}>
+            <a href={repo.url}>
+              {repo.url} / {repo.name}
+            </a>
+            <div style={{ fontWeight: "bold" }}>{repo.description}</div>
+            <div>{repo.forks} forks</div>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default App;
