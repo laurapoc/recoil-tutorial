aeae2f87ffc881968aa4bec5c019e74fc859e654
Laura Pociuniene <laurapociuniene@gmail.com>
2021-11-14 14:51:40 +0200

    fetch data, add Recoil package

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    const getRepos = async () => {
+      const url = "https://api.github.com/users/laurapoc/repos";
+      const resp = await fetch(url);
+      const body = await resp.json();
+      console.log(body);
+    };
+
+    getRepos();
+  }, []);
+
+  return <div></div>;
 }
 
 export default App;
